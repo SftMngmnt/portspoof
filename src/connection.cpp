3529284e6cfaa52d5b9b96a9be00683a9333b436
kg3 <kurt3rd@gmail.com>
2016-10-23 19:19:37 -0500

    [ finding spot to print ip only once  ]

--- src/connection.cpp
+++ src/connection.cpp
@@ -112,31 +112,7 @@ void* process_connection(void *arg)
 	tv.tv_sec = 1;
 	tv.tv_usec = 0;
 
-	int same_newsockfd;
-	string temp = "0.0.0.0", compare_me ;
-
 	while(1) {
-
-		/**
-		 * trying to single out ONE ip per connection in thread pool.
-		 * Putting inside mutex just incase.
-		 */
-		get_ipstr(threads[tid].clients[0], ipstr);
-		compare_me = string(ipstr);
-		if( temp.compare(compare_me) != 0 )
-		{
-			get_ipstr(threads[tid].clients[0], ipstr);
-			temp = string(ipstr);
-			fprintf(stdout,"\nnew connection: %s",ipstr );
-			//fprintf(stdout,"\nDEBUG: TID: %d #Connections: %d",choosen,threads[choosen].client_count);
-
-			/**
-			 * immediate blacklisting can be done here
-			 * sending ip directly into ipset would be sufficient?
-			 * threads would still be running but this maybe not the most sufficient spot
-			 */
-			Utils::forking("echo sending in ipset add " + temp);
-		}
 		
 		sleep(1);
 		for(int i = 0; i < MAX_CLIENT_PER_THREAD; i++)
